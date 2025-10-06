be4681c49fa0cfb3c80e3aea4356e3a3fde4c99a
Eckhard Völlm <e.voellm@mail.de>
2025-10-06 12:09:57 +0200

    key repeat implemented for Up/Down buttons

--- main/Switch.cpp
+++ main/Switch.cpp
@@ -1,200 +1,160 @@
-/*
- * Switch.cpp
- *
- *  Created on: Nov 11, 2023
- *      Author: iltis
- *
- *      Static class for a single push buttom
- *
- *      Example:
- *      sw = Switch();
- *      sw.begin( GPIO_NUM_1 );
- *      if( sw.state() )
- *         ESP_LOGI(FNAME,"Open");
- *
- */
-
+#include "Switch.h"
 #include "esp32-hal.h"
 #include "driver/gpio.h"
-#include "Switch.h"
-#include <logdef.h>
-#include "Setup.h"
-#include "average.h"
-#include "vector.h"
-#include "Units.h"
-#include "flarmview.h"
+#include <algorithm>
+#include <esp_log.h>
 
+#define FNAME "Switch"
 
+std::list<SwitchObserver*> Switch::observers;
+std::list<Switch*> Switch::instances;
+TaskHandle_t Switch::pid = nullptr;
 
-std::list<SwitchObserver *> Switch::observers;
-TaskHandle_t Switch::pid;
+#define REPEAT_DELAY_MS 500     // Time to first repeat
+#define REPEAT_RATE_MS  200     // Repeat intervall
 
-#define TASK_PERIOD 10
+Switch::Switch() {
+    _sw = GPIO_NUM_0;
+    _mode = B_MODE;
+    _state = B_IDLE;
+    _holddown = 0;
+    p_time = 0;
+    repeat_timer = 0;
+    repeating = false;
 
-Switch::Switch( ) {
-	_sw = GPIO_NUM_0;
-	_mode = B_MODE;
-	_closed = false;
-	_holddown = 0;
-	_tick = 0;
-	_closed_timer = 0;
-	_long_timer = 0;
-	pid = 0;
-	_click_timer=0;
-	_clicks=0;
-	_state=B_IDLE;
-	p_time = 0;
-	r_time = 0;
+    // Registriere die Instanz automatisch für Task
+    instances.push_back(this);
 }
 
 Switch::~Switch() {
+    // Entferne Instanz aus Liste
+    instances.remove(this);
 }
 
-
-void Switch::attach(SwitchObserver *obs) {
-	// ESP_LOGI(FNAME,"Attach obs: %p", obs );
-	observers.push_back(obs);
-}
-
-void Switch::detach(SwitchObserver *obs) {
-	// ESP_LOGI(FNAME,"Detach obs: %p", obs );
-	/*
-        auto it = std::find(observers.begin(), observers.end(), obs);
-        if ( it != observers.end() ) {
-                observers.erase(it);
-        }
-	 */
-}
-
-void Switch::switchTask(void *pvParameters){
-	while(1){
-		swUp.tick();
-		swDown.tick();
-		swMode.tick();
-		delay(TASK_PERIOD);
-	}
-}
-
-void Switch::startTask(){
-	ESP_LOGI(FNAME,"taskStart");
-	xTaskCreatePinnedToCore(&switchTask, "Switch", 6096, NULL, 9, &pid, 0);
-}
-
-void Switch::begin( gpio_num_t sw, t_button mode ){
-	ESP_LOGI(FNAME,"Switch::begin GPIO: %d", sw);
-	_sw = sw;
-	_mode = mode;
-	gpio_set_direction(_sw, GPIO_MODE_INPUT);
-	gpio_set_pull_mode(_sw, GPIO_PULLUP_ONLY);
-
+void Switch::begin(gpio_num_t sw, t_button mode) {
+    _sw = sw;
+    _mode = mode;
+    gpio_set_direction(_sw, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(_sw, GPIO_PULLUP_ONLY);
 }
 
 bool Switch::isClosed() {
-
-	int level = gpio_get_level(_sw );
-	if( level )
-		return false;
-	else
-		return true;
+    return gpio_get_level(_sw) == 0;
 }
 
 bool Switch::isOpen() {
-	return( !isClosed() );
+    return !isClosed();
 }
 
+void Switch::attach(SwitchObserver* obs) {
+    observers.push_back(obs);
+}
+
+void Switch::detach(SwitchObserver* obs) {
+    auto it = std::find(observers.begin(), observers.end(), obs);
+    if (it != observers.end()) observers.erase(it);
+}
+
+void Switch::notifyObserversPress() {
+    for (auto& obs : observers) {
+        switch (_mode) {
+            case B_MODE: obs->press(); break;
+            case B_UP: obs->up(1); break;
+            case B_DOWN: obs->down(1); break;
+        }
+    }
+}
+
+void Switch::sendPress(int dur) {
+    ESP_LOGI(FNAME, "Press (%d ms)", dur);
+    notifyObserversPress();
+}
+
+void Switch::sendLongPress(int dur) {
+    ESP_LOGI(FNAME, "Long Press (%d ms)", dur);
+    if (_mode == B_MODE) for (auto& obs : observers) obs->longPress();
+    else if (_mode == B_UP) for (auto& obs : observers) obs->up(1);
+    else if (_mode == B_DOWN) for (auto& obs : observers) obs->down(1);
+}
+
+void Switch::sendLongLongPress(int dur) {
+    ESP_LOGI(FNAME, "Long Long Press (%d ms)", dur);
+    if (_mode == B_MODE) for (auto& obs : observers) obs->longLongPress();
+    else if (_mode == B_UP) for (auto& obs : observers) obs->up(1);
+    else if (_mode == B_DOWN) for (auto& obs : observers) obs->down(1);
+}
 
 void Switch::tick() {
-	unsigned long currentMillis = millis();
-	// ESP_LOGI(FNAME,"tick %ld millis", currentMillis );
-	if( _holddown){
-		_holddown--;
-	}
-	else{
-		switch (_state) {
-		case B_IDLE:
-			if (isClosed()) {  // Button pressed
-				_state = B_PRESSED;
-				p_time = currentMillis;
-				ESP_LOGI(FNAME,"PRESSED" );
-			}
-			break;
+    unsigned long currentMillis = millis();
 
-		case B_PRESSED:
-			if (isOpen()) {
-				int dur = currentMillis - p_time;
-				// Button pressed longer as 300 mS
-				if (dur < 300) {
-					sendPress(dur);
-					_holddown = 5;  // 50 mS debounce
-				}
-				else if (dur > 300 && dur < 2000 ) {
-					sendLongPress(dur);
-					_holddown = 5;
-				}
-				_state = B_IDLE;
-			}
-			else{
-				int dur = currentMillis - p_time;
-				// ESP_LOGI(FNAME,"STILL CLOSED, but dur=%d",dur );
-				if (dur > 2000) {   // we implement this as a long long click
-					sendLongLongPress(dur);
-					_holddown = 5;
-					_state = B_PRESSED_STILL;
-				}
-			}
-			break;
+    if (_holddown > 0) {
+        _holddown--;
+        return;
+    }
 
-		case B_PRESSED_STILL:
-			if (isOpen()) {
-				_state = B_IDLE;
-				_holddown = 5;
-			}
-			break;
-		}
-	}
+    switch (_state) {
+        case B_IDLE:
+            if (isClosed()) {
+                _state = B_PRESSED;
+                p_time = currentMillis;
+                ESP_LOGI(FNAME, "Button PRESSED");
+            }
+            break;
+
+        case B_PRESSED:
+        	if (isOpen()) {
+        		int dur = currentMillis - p_time;
+        		if (dur < LONG_PRESS_MS) sendPress(dur);
+        		else if (dur < LONG_LONG_PRESS_MS) sendLongPress(dur);
+        		_holddown = DEBOUNCE_MS / TASK_PERIOD_MS;
+        		_state = B_IDLE;
+        		repeating = false;        // Stop Repeat when released
+        	} else {
+        		int dur = currentMillis - p_time;
+
+        		// PC-Key like repeat only for B_UP / B_DOWN buttons
+        		if ((_mode == B_UP || _mode == B_DOWN)) {
+        			if (!repeating && dur >= REPEAT_DELAY_MS) {
+        				repeating = true;
+        				repeat_timer = currentMillis + REPEAT_RATE_MS;
+        				notifyObserversPress();
+        			}
+        			if (repeating && currentMillis >= repeat_timer) {
+        				notifyObserversPress();
+        				repeat_timer = currentMillis + REPEAT_RATE_MS;
+        			}
+        		}
+
+        		// Long long Press (or hold) für B_MODE
+        		if (_mode == B_MODE && dur > LONG_LONG_PRESS_MS) {
+        			sendLongLongPress(dur);
+        			_holddown = DEBOUNCE_MS / TASK_PERIOD_MS;
+        			_state = B_PRESSED_STILL;
+        		}
+        	}
+        	break;
+
+
+        case B_PRESSED_STILL:
+            if (isOpen()) {
+                _state = B_IDLE;
+                _holddown = DEBOUNCE_MS / TASK_PERIOD_MS;
+            }
+            break;
+    }
 }
 
-void Switch::sendPress(int dur){
-	ESP_LOGI(FNAME,"send press (%d ms)", dur);
-	for (auto &observer : observers){
-		if( _mode == B_MODE ){
-			observer->press();
-		}
-		else if( _mode == B_UP ){
-			observer->up(1);
-		}
-		else if( _mode == B_DOWN ){
-			observer->down(1);
-		}
-	}
+void Switch::switchTask(void* pvParameters) {
+    while (1) {
+        for (auto& sw : instances) {
+            sw->tick();
+        }
+        vTaskDelay(pdMS_TO_TICKS(TASK_PERIOD_MS));
+    }
 }
 
-void Switch::sendLongPress(int dur){
-	ESP_LOGI(FNAME,"send longPress (%d ms)", dur);
-	for (auto &observer : observers)
-		if( _mode == B_MODE ){
-			observer->longPress();
-		}
-		else if( _mode == B_UP ){
-			observer->up(1);
-		}
-		else if( _mode == B_DOWN ){
-			observer->down(1);
-		}
+void Switch::startTask() {
+    ESP_LOGI(FNAME, "Starting Switch Task");
+    if (pid == nullptr)
+        xTaskCreatePinnedToCore(&switchTask, "Switch", 6096, NULL, 9, &pid, 0);
 }
-
-void Switch::sendLongLongPress(int dur){
-	ESP_LOGI(FNAME,"send long long press (%d ms)", dur);
-	for (auto &observer : observers)
-		if( _mode == B_MODE ){
-			observer->longLongPress();
-		}
-		else if( _mode == B_UP ){
-			observer->up(1);
-		}
-		else if( _mode == B_DOWN ){
-			observer->down(1);
-		}
-}
-
-
